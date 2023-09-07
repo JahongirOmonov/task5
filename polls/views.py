8be4034a5a0411a2a50825eb3f753ecca07662dd
Jahongir <dodajoy0777@gmail.com>
2023-09-07 07:26:38 +0500

    ss

--- polls/views.py
+++ polls/views.py
@@ -3,18 +3,26 @@ from django.shortcuts import render
 from .models import maktab, xonalar
 from django.http import JsonResponse
 from .serializer import maktabSerializer, xonalarSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
-def all(request):
-    all=maktab.objects.all()
-    result=maktabSerializer(all, many=True)
-    # for i in obshi:
-    #     allitem.append({
-    #         'name':i.name,
-    #         'oquvchilar_soni':i.oquvchilar_soni
-    #     })
-    return JsonResponse(result.data, safe=False)
+# def all(request):
+#     all=maktab.objects.all()
+#     result=maktabSerializer(all, many=True)
+#     # for i in obshi:
+#     #     allitem.append({
+#     #         'name':i.name,
+#     #         'oquvchilar_soni':i.oquvchilar_soni
+#     #     })
+#     return JsonResponse(result.data, safe=False)
+
+class getmaktab(APIView):
+    def get(self, request):
+        good = maktab.objects.all()
+        serkalaz=maktabSerializer(good, many=True)
+        return Response(serkalaz.data)
     
 
 
@@ -22,4 +30,13 @@ def detail(request, shuid):
     some = xonalar.objects.filter(id=myid)
     forgett = xonalarSerializer(some, many=True)
     return JsonResponse(forgett, safe=False)
+
+class psotmaktab(APIView):
+    def post(self, request):
+        asosiy_qism=request.data
+        serialize = fullnameSerializer(data=asosiy_qism)
+        if serialize.is_valid():
+            serialize.save()
+            return Response(serialize.data)
+        return Response(serialize.errors)
         
